4274b1736fb67ebd28e55ee45021a7caa3e3d8e1
Qspiders Wakad <Qspiders Wakad@LAPTOP-KNL22F4G>
2023-07-20 16:21:09 +0530

    commit

--- wcsa5seleniumproject/src/testNGAnnotation_Flags/Flag2.java
+++ wcsa5seleniumproject/src/testNGAnnotation_Flags/Flag2.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 public class Flag2 {
 	
+	// it is use to disable or enable the methods/TestCase
   @Test(description = "Login Method!!",enabled = true)
   public void method1() {
 	  Reporter.log("Method1 from falg2",true);
